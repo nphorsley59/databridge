96b5a82384ad3e9e061b0d20d880aea34fbe44db
Noah Horsley <nphorsley59@gmail.com>
2024-06-30 12:24:38 -0400

    Write Directory class for path management

---
+++ adapters/config.py
@@ -0,0 +1,9 @@
+
+from pathlib import Path
+
+
+class Directory:
+    BASE = Path(__file__).resolve().parent
+    DATA = BASE / "data"
+    TEST_ASSETS = DATA / "test_assets"
+    TEST_GALLERY = DATA / "test_gallery"
